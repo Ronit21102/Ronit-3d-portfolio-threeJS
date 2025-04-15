d4509a079e8c600b9124acfb2ebb27eb7ebfeab9
ronit21102-js <ronit21102@gmail.com>
2025-04-16 01:06:14 +0530

    first commit

---
+++ src/constants/kreedaLogo.js
@@ -0,0 +1,8 @@
+export const kreedaLogo =
+  "https://static.wixstatic.com/media/2d791f_3d00420938fc4862ae8d48ed699d6e2f~mv2.png/v1/fill/w_403,h_31,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/2d791f_3d00420938fc4862ae8d48ed699d6e2f~mv2.png";
+
+export const springWorks =
+  "https://cdn.prod.website-files.com/5d7f260f28eccad078dcb101/633e70c2a9588e1ae6210aaf_sw-logo.svg";
+
+export const codingNinjas =
+  "https://files.codingninjas.in/new_cn_logo-29829.svg";
